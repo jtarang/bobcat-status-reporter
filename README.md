a65d0ff13557b40a5562f0de41e0cd7cd3d06418
jaytarang92 <jaytarang92@gmail.com>
2021-09-12 22:54:42 -0500

    update readme doc and reformat code

--- README.md
+++ README.md
@@ -13,6 +13,26 @@ temperature be below 60C. I just wanted to make sure the temp did not exceed tha
 ## Setup Environment
 - Install Requirements ```pip install -r requirements.txt```
 
+## Arguments
+```shell
+
+Usage: report_miner_status.py [OPTIONS] MINER_IP_ADDRESS
+                              GOOGLE_SHEETS_CREDENTIALS_FILE_PATH
+                              GOOGLE_SHEET_ID
+
+Arguments:
+  MINER_IP_ADDRESS                [required]
+  GOOGLE_SHEETS_CREDENTIALS_FILE_PATH
+                                  [required]
+  GOOGLE_SHEET_ID                 [required]
+
+Options:
+  --worksheet-id INTEGER
+  --clear-worksheet / --no-clear-worksheet
+                                  [default: False]
+  --help                          Show this message and exit.
+```
+
 ## Usage
 ```python3 
 python report_miner_status.py 192.168.50.155 ./.credentials/bobcat-miner-service-account.json BobcatMinerReport
--- report_miner_status.py
+++ report_miner_status.py
@@ -1,4 +1,5 @@
 import typer
+
 from src.bobcat_client.client import BobcatClient
 from src.google_sheets.client import GoogleSheetsClient
 
@@ -6,10 +7,24 @@ from src.google_sheets.client import GoogleSheetsClient
 Usage: report_miner_status.py [OPTIONS] MINER_IP_ADDRESS
                               GOOGLE_SHEETS_CREDENTIALS_FILE_PATH
                               GOOGLE_SHEET_ID
-Try 'report_miner_status.py --help' for help.
 
-Error: Missing argument 'MINER_IP_ADDRESS'.
+Arguments:
+  MINER_IP_ADDRESS                [required]
+  GOOGLE_SHEETS_CREDENTIALS_FILE_PATH
+                                  [required]
+  GOOGLE_SHEET_ID                 [required]
 
+Options:
+  --worksheet-id INTEGER
+  --clear-worksheet / --no-clear-worksheet
+                                  [default: False]
+  --install-completion [bash|zsh|fish|powershell|pwsh]
+                                  Install completion for the specified shell.
+  --show-completion [bash|zsh|fish|powershell|pwsh]
+                                  Show completion for the specified shell, to
+                                  copy it or customize the installation.
+
+  --help                          Show this message and exit.
 """
 
 cli = typer.Typer()
@@ -21,7 +36,7 @@ def report_status(miner_ip_address: str, google_sheets_credentials_file_path: st
     google_sheets_client = GoogleSheetsClient(
         google_sheets_service_account_file_path=google_sheets_credentials_file_path)
     bobcat_client = BobcatClient(miner_ip_address=miner_ip_address)
-    miner_report = {**bobcat_client.get_temperature().__dict__} # **bobcat_client.get_sync_status().__dict__}
+    miner_report = {**bobcat_client.get_temperature().__dict__}  # **bobcat_client.get_sync_status().__dict__}
     google_sheets_client.write_to_sheet(sheet_id=google_sheet_id, data=miner_report, worksheet_id=worksheet_id,
                                         clear_worksheet=clear_worksheet)
     return None

ba9e7804e3bdda8beba40eb52b9d41f93b30eb77
jaytarang92 <jaytarang92@gmail.com>
2021-09-13 22:20:40 -0500

    add sync_status to report

--- report_miner_status.py
+++ report_miner_status.py
@@ -36,7 +36,9 @@ def report_status(miner_ip_address: str, google_sheets_credentials_file_path: st
     google_sheets_client = GoogleSheetsClient(
         google_sheets_service_account_file_path=google_sheets_credentials_file_path)
     bobcat_client = BobcatClient(miner_ip_address=miner_ip_address)
-    miner_report = {**bobcat_client.get_temperature().__dict__}  # **bobcat_client.get_sync_status().__dict__}
+    # For some reason you have to get `sync_status` first otherwise you get a `rate limit exceeded error`
+    _sync_status = bobcat_client.get_sync_status()
+    miner_report = {**bobcat_client.get_temperature().__dict__, **_sync_status.__dict__}
     google_sheets_client.write_to_sheet(sheet_id=google_sheet_id, data=miner_report, worksheet_id=worksheet_id,
                                         clear_worksheet=clear_worksheet)
     return None

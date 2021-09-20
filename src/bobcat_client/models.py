e64b6ff55cc49f83a9d921e78cc0a74ec0978970
jaytarang92 <jaytarang92@gmail.com>
2021-09-20 00:16:27 -0500

    allow timezone to be configured

--- src/bobcat_client/models.py
+++ src/bobcat_client/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-
+from src.bobcat_client.timezone_handler import TimezoneHandler
 
 @dataclass
 class Temperature:
@@ -8,6 +8,8 @@ class Temperature:
     temp1: int = field(default=None)
     unit: str = field(default=None)
 
+    def convert_timestamp_timezone(self, to_timezone: str):
+        self.timestamp = TimezoneHandler.convert_timezone(timestamp=self.timestamp, to_timezone=to_timezone)
 
 @dataclass
 class SyncStatus:
